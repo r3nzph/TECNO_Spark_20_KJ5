abb0a91b8e4e2fd5a9cb5ba6e64f65ae8bdfc9c7
r3nzph <abarracosorenznino@gmail.com>
2024-09-06 11:48:28 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/TECNO-KJ5
+LOCAL_PATH := device/tecno/KJ5
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
